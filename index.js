9777d9f5ffcfca29fb47e53cd912107b3ad3137f
luix gabriel <luisgasbriel@gmail.com>
2023-01-14 13:21:54 -0300

    adicionando o rukawa

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const sakuragi = document.getElementById('sakuragi')
+const rukawa = document.getElementById('rukawa')
+console.log(rukawa)
 
 sakuragi.addEventListener('mouseenter', ()=>{
     const description = document.querySelector('.description')
@@ -7,6 +9,33 @@ sakuragi.addEventListener('mouseenter', ()=>{
     description.classList.add('animate__fadeInDown')
     // description.setAttribute('class', 'description-show')
 })
+
+sakuragi.addEventListener('mouseout', ()=>{
+    const description = document.querySelector('.description')
+    description.setAttribute('class', 'description')
+    
+    
+})
+
+rukawa.addEventListener('mouseenter', ()=>{
+    const description = document.querySelector('.description-rukawa')
+   
+    // description.classList.add('description-show')
+    // description.classList.add('animate__animated')
+    // description.classList.add('animate__fadeInDown')
+    description.setAttribute('class', 'description-show')
+     description.classList.add('animate__animated')
+     description.classList.add('animate__fadeInDown')
+     
+})
+
+rukawa.addEventListener('mouseout', ()=>{
+    const description = document.querySelector('.description-show')
+    description.setAttribute('class', 'description-rukawa')
+    
+    
+})
+
 
 
 
