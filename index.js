d639127ce82276b9da899cec74c15172c7fd01b1
luix gabriel <luisgasbriel@gmail.com>
2023-01-16 10:07:31 -0300

    Adicionando jogadores

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const sakuragi = document.getElementById('sakuragi')
 const rukawa = document.getElementById('rukawa')
-console.log(rukawa)
+const akagi = document.getElementById('akagi')
+const ryota = document.getElementById('ryota')
+const mitsui = document.getElementById('mitsui')
+
 
 sakuragi.addEventListener('mouseenter', ()=>{
     const description = document.querySelector('.description')
@@ -19,10 +22,6 @@ sakuragi.addEventListener('mouseout', ()=>{
 
 rukawa.addEventListener('mouseenter', ()=>{
     const description = document.querySelector('.description-rukawa')
-   
-    // description.classList.add('description-show')
-    // description.classList.add('animate__animated')
-    // description.classList.add('animate__fadeInDown')
     description.setAttribute('class', 'description-show')
      description.classList.add('animate__animated')
      description.classList.add('animate__fadeInDown')
@@ -32,6 +31,51 @@ rukawa.addEventListener('mouseenter', ()=>{
 rukawa.addEventListener('mouseout', ()=>{
     const description = document.querySelector('.description-show')
     description.setAttribute('class', 'description-rukawa')
+    
+    
+})
+
+akagi.addEventListener('mouseenter', ()=>{
+    const description = document.querySelector('.description-akagi')
+     description.setAttribute('class', 'description-show')
+     description.classList.add('animate__animated')
+     description.classList.add('animate__fadeInDown')
+     
+})
+
+akagi.addEventListener('mouseout', ()=>{
+    const description = document.querySelector('.description-show')
+    description.setAttribute('class', 'description-akagi')
+    
+    
+})
+
+ryota.addEventListener('mouseenter', ()=>{
+    const description = document.querySelector('.description-ryota')
+    description.setAttribute('class', 'description-show')
+     description.classList.add('animate__animated')
+     description.classList.add('animate__fadeInDown')
+     
+})
+
+ryota.addEventListener('mouseout', ()=>{
+    const description = document.querySelector('.description-show')
+    description.setAttribute('class', 'description-ryota')
+    
+    
+})
+
+mitsui.addEventListener('mouseenter', ()=>{
+    const description = document.querySelector('.description-mitsui')
+    description.setAttribute('class', 'description-show')
+     description.classList.add('animate__animated')
+     description.classList.add('animate__fadeInDown')
+     
+})
+
+mitsui.addEventListener('mouseout', ()=>{
+    const description = document.querySelector('.description-show')
+    description.setAttribute('class', 'description-mitsui')
     
     
 })
